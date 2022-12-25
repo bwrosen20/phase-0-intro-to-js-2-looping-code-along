17fbc9e85d470a2d2e89af122970b0509363b866
bwrosen20 <bwrosen20@gmail.com>
2022-12-24 21:25:24 -0500

    commit tree 7c9dfc80cf387133021511400e052baa93e389c2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names){
+    let counter=0;
+    let namesArray=[...names];
+    while (counter<names.length){
+        namesArray[counter]=`Thank you, ${names[counter]}, for the wonderful surprise gift!`;
+        counter++
+    }
+    return namesArray;
+}
+function countDown(number){
+    while (number>=0){
+    console.log(number);
+    number--;
+    }
+}
